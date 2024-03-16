ce9a28c662551bd7b6d9b1345c50c46a610aaf6a
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2024-03-16 13:36:01 +0100

    Added mysql function to create uuid v4

--- database/v1.0.0.sql
+++ database/v1.0.0.sql
@@ -1,10 +1,10 @@
 create table users (
-  id varchar(36) not null primary key
+  id char(36) not null primary key
 );
 
 create table categories (
-  id varchar(36) not null primary key,
-  user_id varchar(36),
+  id char(36) not null primary key,
+  user_id char(36),
   name text not null,
   
   index ix_user_id (user_id),
@@ -27,8 +27,8 @@ insert into categories (id, user_id, name) values
   ('21d64468-61d3-455e-ac4e-90baddf540a7', null, 'Animali domestici');
 
 create table receipts (
-  id varchar(36) not null primary key,
-  user_id varchar(36) not null,
+  id char(36) not null primary key,
+  user_id char(36) not null,
   `date` date,
   total_amount decimal(10, 2) not null default 0,
   store_name text not null,
@@ -45,11 +45,11 @@ create table receipts (
 );
 
 create table receipt_item (
-  id varchar(36) not null primary key,
-  receipt_id varchar(36) not null,
+  id char(36) not null primary key,
+  receipt_id char(36) not null,
   description text not null,
   amount decimal(10, 2) not null default 0,
-  category_id varchar(36),
+  category_id char(36),
   
   index ix_receipt_id (receipt_id),
   index ix_category_id (category_id),
@@ -64,3 +64,33 @@ create table receipt_item (
     on delete restrict
     on update restrict    
 );
+
+DELIMITER //
+
+CREATE FUNCTION uuid_v4()
+    RETURNS CHAR(36) NO SQL
+BEGIN
+    -- Generate 8 2-byte strings that we will combine into a UUIDv4
+    SET @h1 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h2 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h3 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h6 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h7 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+    SET @h8 = LPAD(HEX(FLOOR(RAND() * 0xffff)), 4, '0');
+
+    -- 4th section will start with a 4 indicating the version
+    SET @h4 = CONCAT('4', LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
+
+    -- 5th section first half-byte can only be 8, 9 A or B
+    SET @h5 = CONCAT(HEX(FLOOR(RAND() * 4 + 8)),
+                LPAD(HEX(FLOOR(RAND() * 0x0fff)), 3, '0'));
+
+    -- Build the complete UUID
+    RETURN LOWER(CONCAT(
+        @h1, @h2, '-', @h3, '-', @h4, '-', @h5, '-', @h6, @h7, @h8
+    ));
+END
+//
+
+DELIMITER ;
+
