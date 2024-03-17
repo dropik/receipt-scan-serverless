4982e04835a4cfbd164ef76e806f1a75a8b54388
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2024-03-17 19:18:03 +0100

    Factored out lambda handler into a class

--- receipt-recognition-service/src/main.cpp
+++ receipt-recognition-service/src/main.cpp
@@ -1,3 +1,4 @@
+#include <aws/core/utils/memory/stl/AWSAllocator.h>
 #include <aws/lambda-runtime/runtime.h>
 
 #include <aws/core/Aws.h>
@@ -12,6 +13,7 @@
 #include <aws/ssm/SSMClient.h>
 #include <aws/ssm/SSMServiceClientModel.h>
 #include <aws/ssm/model/GetParameterRequest.h>
+#include <aws/textract/TextractClient.h>
 
 #include "conncpp/Connection.hpp"
 #include "conncpp/DriverManager.hpp"
@@ -21,6 +23,7 @@
 #include "conncpp/Statement.hpp"
 
 #include <aws-lambda-cpp/common/logger.hpp>
+#include <memory>
 
 #include "config.h"
 
@@ -31,34 +34,48 @@ using namespace Aws::Utils::Logging;
 using namespace Aws::Utils::Json;
 using namespace aws_lambda_cpp::common;
 
-invocation_response lambda_handler(const logger& logger, invocation_request const& request) {
-  logger.info("Version %s", VERSION);
-
-  try {
-    logger.info("Establishing connection with the database...");
-    
-    sql::SQLString url(connection_string);
-    std::unique_ptr<sql::Connection> conn(sql::DriverManager::getConnection(url));
-    if (conn == nullptr) {
-      logger.error("Unable to establish connection with database!");
-      return invocation_response::failure("{\"statusCode\": 500}", "application/json");
+class receipt_recognition_service {
+  public:
+    receipt_recognition_service(
+        const std::shared_ptr<const Aws::Textract::TextractClient>& textractClient,
+        const std::shared_ptr<const logger>& logger) {
+      this->_logger = logger;
+      this->_textractClient = textractClient;
     }
 
-    logger.info("Successfully connected to datbase!");
-    logger.info("Reading from database!");
+    invocation_response handle_request(invocation_request const& request) {
+      _logger->info("Version %s", VERSION);
 
-    std::unique_ptr<sql::Statement> stmnt(conn->createStatement());
-    sql::ResultSet* res = stmnt->executeQuery("select count(1) from users u");
-    res->next();
-    logger.info("Found users: %d", res->getInt(1));
+      try {
+        _logger->info("Establishing connection with the database...");
     
-    conn->close();
-  } catch (sql::SQLException& e) {
-    logger.error("Error occured while doing operation on database: %s", e.what());
-  }
+        sql::SQLString url(connection_string);
+        std::unique_ptr<sql::Connection> conn(sql::DriverManager::getConnection(url));
+        if (conn == nullptr) {
+          _logger->error("Unable to establish connection with database!");
+          return invocation_response::failure("{\"statusCode\": 500}", "application/json");
+        }
 
-  return invocation_response::success("{\"statusCode\": 200}", "application/json");
-}
+        _logger->info("Successfully connected to datbase!");
+        _logger->info("Reading from database!");
+
+        std::unique_ptr<sql::Statement> stmnt(conn->createStatement());
+        sql::ResultSet* res = stmnt->executeQuery("select count(1) from users u");
+        res->next();
+        _logger->info("Found users: %d", res->getInt(1));
+    
+        conn->close();
+      } catch (sql::SQLException& e) {
+        _logger->error("Error occured while doing operation on database: %s", e.what());
+      }
+
+      return invocation_response::success("{\"statusCode\": 200}", "application/json");
+    }
+
+  private:
+    std::shared_ptr<const logger> _logger;
+    std::shared_ptr<const Aws::Textract::TextractClient> _textractClient;
+};
 
 std::function<std::shared_ptr<LogSystemInterface>()> GetConsoleLoggerFactory() {
   return [] {
@@ -77,7 +94,8 @@ int main() {
 
   InitAPI(options);
   {
-    logger logger("receipt-recognition-service");
+    std::shared_ptr<logger> l = std::make_shared<logger>("receipt-recognition-service");
+    Aws::Client::ClientConfiguration config;
 
 #ifdef DEBUG
       connection_string = getenv("DB_CONNECTION_STRING");
@@ -90,7 +108,6 @@ int main() {
       logger.info("Running on stage %s", stage.c_str());
 
       std::string ssmPrefix = str_format("/receipt-scan/%s", stage.c_str());
-      Aws::Client::ClientConfiguration config;
       Aws::SSM::SSMClient ssmClient(config);
       Aws::SSM::Model::GetParameterRequest connStrReq;
       connStrReq
@@ -105,8 +122,11 @@ int main() {
       connection_string = outcome.GetResult().GetParameter().GetValue();
 #endif
 
+      std::shared_ptr<Aws::Textract::TextractClient> textractClient = Aws::MakeShared<Aws::Textract::TextractClient>("textract_client", config);
+
+      receipt_recognition_service handler = receipt_recognition_service(textractClient, l);
       run_handler([&](const invocation_request& req) {
-        return lambda_handler(logger, req);
+        return handler.handle_request(req);
       });
   }
   ShutdownAPI(options);
