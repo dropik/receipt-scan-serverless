d316c18420b6a438ba912efa0211d81117d232f4
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2024-05-18 20:27:41 +0200

    Configured header templates

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,9 @@
+#if ($HEADER_COMMENTS)
+//
+// Created by Daniil Ryzhkov on ${DATE}.
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+//
+#end
+
